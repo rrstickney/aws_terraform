8fe771847921da9ea77569269b55a0af080c629f
Ryan Stickney <rstickney@icestation.is>
2024-01-31 18:41:41 -0800

    setting up networks

---
+++ workspaces/vpc/modules/aws/vpc/main.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+}
---
+++ workspaces/vpc/provider.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  profile = "icestation"
+  region  = "us-west-2"
+}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket         = "tfstate-icestation"
+    key            = "vpc/terraform.state"
+    region         = "us-west-2"
+    dynamodb_table = "tfstate-icestation-dynamo"
+  }
+}
