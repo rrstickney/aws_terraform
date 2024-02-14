47affaaef4e2ba7521d8a142d98ad52b64351b1f
Ryan Stickney <rstickney@icestation.is>
2024-02-14 15:26:00 -0800

    fixing a tagging issue and repetitive tagging

--- workspaces/vpc/modules/aws/vpc/output.tf
+++ workspaces/vpc/modules/aws/vpc/output.tf
@@ -1,13 +1,3 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-
-output "public_subnets_id" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "private_subnets_id" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
-}
