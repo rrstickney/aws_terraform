5711f42e18a4f90bcaddbaafa6656970a6f724c1
Ryan Stickney <rstickney@icestation.is>
2024-02-15 17:48:22 -0800

    improving tagging and using a single nat gateway

--- workspaces/vpc/locals.tf
+++ workspaces/vpc/locals.tf
@@ -6,6 +6,7 @@ locals {
       private_subnets    = ["10.141.1.0/24", "10.141.2.0/24", "10.141.3.0/24"]
       public_subnets     = ["10.141.101.0/24", "10.141.102.0/24", "10.141.103.0/24"]
       enable_nat_gateway = true
+      single_nat_gateway = true
     }
   }
   tags = {
