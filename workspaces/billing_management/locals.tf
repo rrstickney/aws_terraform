3a7d9ef4a504dbfea8564dcc0fb0211d4ec8a301
Ryan Stickney <rstickney@icestation.is>
2024-02-22 11:00:43 -0800

    setting up billing notifications

---
+++ workspaces/billing_management/locals.tf
@@ -0,0 +1,28 @@
+locals {
+  budgets = {
+    broke_ass_budgeting = {
+      budget_type       = "COST"
+      limit_amount      = 30
+      limit_unit        = "USD"
+      time_period_end   = "2025-01-01_00:00"
+      time_period_start = "2024-03-01_00:00"
+      time_unit         = "MONTHLY"
+      notification = [
+        {
+          comparison_operator        = "GREATER_THAN"
+          threshold                  = 75
+          threshold_type             = "PERCENTAGE"
+          notification_type          = "FORECASTED"
+          subscriber_email_addresses = ["budget_warnings@icestation.is"]
+        },
+        {
+          comparison_operator        = "GREATER_THAN"
+          threshold                  = 90
+          threshold_type             = "PERCENTAGE"
+          notification_type          = "FORECASTED"
+          subscriber_email_addresses = ["budget_critical@icestation.is"]
+        }
+      ]
+    }
+  }
+}
