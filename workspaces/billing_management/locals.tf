0aa84657646f8760cce2ba25c0ce876dcd32146b
Ryan Stickney <rstickney@icestation.is>
2024-03-04 14:43:17 -0800

    making separate actual and forecasted budgets

--- workspaces/billing_management/locals.tf
+++ workspaces/billing_management/locals.tf
@@ -1,6 +1,6 @@
 locals {
   budgets = {
-    broke_ass_budgeting = {
+    broke_ass_budgeting_actual = {
       budget_type       = "COST"
       limit_amount      = 30
       limit_unit        = "USD"
@@ -20,6 +20,30 @@ locals {
           threshold                  = 90
           threshold_type             = "PERCENTAGE"
           notification_type          = "ACTUAL"
+          subscriber_email_addresses = ["budget_critical@icestation.is"]
+        }
+      ]
+    }
+    broke_ass_budgeting_forecasted = {
+      budget_type       = "COST"
+      limit_amount      = 100
+      limit_unit        = "USD"
+      time_period_end   = "2025-01-01_00:00"
+      time_period_start = "2024-03-01_00:00"
+      time_unit         = "MONTHLY"
+      notification = [
+        {
+          comparison_operator        = "GREATER_THAN"
+          threshold                  = 50
+          threshold_type             = "PERCENTAGE"
+          notification_type          = "FORECASTED"
+          subscriber_email_addresses = ["budget_warnings@icestation.is"]
+        },
+        {
+          comparison_operator        = "GREATER_THAN"
+          threshold                  = 75
+          threshold_type             = "PERCENTAGE"
+          notification_type          = "FORECASTED"
           subscriber_email_addresses = ["budget_critical@icestation.is"]
         }
       ]
