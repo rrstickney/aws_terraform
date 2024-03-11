6b3bf250890c897e22b6dd2199ae02849046e28e
Ryan Stickney <rstickney@icestation.is>
2024-03-11 12:57:27 -0700

    setting up OIDC for terraform cloud auth and role for terraform assume to use

--- workspaces/iam_roles/locals.tf
+++ workspaces/iam_roles/locals.tf
@@ -1,17 +1,17 @@
 locals {
   roles = {
     "icestation_admin" = {
-      name                = "icestation_admin"
-      assumed_role_policy = file("${path.module}/files/assume_role_google_workspaces.json")
+      name               = "icestation_admin"
+      assume_role_policy = file("${path.module}/files/assume_role_google_workspaces.json")
       managed_policy_arns = [
         "arn:aws:iam::aws:policy/AdministratorAccess"
       ]
     }
     "terraform_cloud_admin" = {
-      name                = "terraform_cloud_admin"
-      assumed_role_policy = file("${path.module}/files/assume_role_terraform_cloud.json")
+      name               = "terraform_cloud_admin"
+      assume_role_policy = file("${path.module}/files/assume_role_terraform_cloud.json")
       managed_policy_arns = [
-        ""
+        "arn:aws:iam::aws:policy/AdministratorAccess"
       ]
     }
   }
