d95414ba60fa1e0b26eaef6a91c632d5f9762eac
Ryan Stickney <rstickney@icestation.is>
2024-03-18 12:33:42 -0700

    updating to use terraform cloud as remote state

--- workspaces/vpc/provider.tf
+++ workspaces/vpc/provider.tf
@@ -2,15 +2,22 @@ provider "aws" {
   region = "us-west-2"
 }
 terraform {
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "icestation-is"
+    workspaces {
+      tags = ["vpc"]
+    }
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
     }
   }
-  backend "s3" {
-    bucket         = "tfstate-icestation"
-    key            = "vpc/terraform.state"
-    region         = "us-west-2"
-    dynamodb_table = "tfstate-icestation-dynamo"
-  }
+  # backend "s3" {
+  #   bucket         = "tfstate-icestation"
+  #   key            = "vpc/terraform.state"
+  #   region         = "us-west-2"
+  #   dynamodb_table = "tfstate-icestation-dynamo"
+  # }
 }
