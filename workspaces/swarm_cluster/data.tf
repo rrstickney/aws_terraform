1be4039f036e12322b7b3a2a309b36c0166ebc6c
Ryan Stickney <rstickney@icestation.is>
2024-03-18 12:56:31 -0700

    updating datasources to use tfe_outputs

--- workspaces/swarm_cluster/data.tf
+++ workspaces/swarm_cluster/data.tf
@@ -1,58 +1,3 @@
-# for future use, commenting out since it is necessary for current needs# get my external IP address for adding to ingress rules in SGs
-# data "http" "my_ip" {
-#   url = "http://icanhazip.com"
-# }
-
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  config = {
-    bucket         = "tfstate-icestation"
-    key            = "vpc/terraform.state"
-    region         = "us-west-2"
-    dynamodb_table = "tfstate-icestation-dynamo"
-  }
-}
-
-data "aws_subnet" "public_subnet_2a" {
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_output.main_vpc.vpc_output.vpc_id
-
-  filter {
-    name   = "tag:Name"
-    values = ["public_subnet"]
-  }
-  filter {
-    name   = "availability-zone"
-    values = ["us-west-2a"]
-  }
-}
-
-data "aws_subnet" "public_subnet_2b" {
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_output.main_vpc.vpc_output.vpc_id
-
-  filter {
-    name   = "tag:Name"
-    values = ["public_subnet"]
-  }
-  filter {
-    name   = "availability-zone"
-    values = ["us-west-2b"]
-  }
-}
-
-data "aws_subnet" "public_subnet_2c" {
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_output.main_vpc.vpc_output.vpc_id
-
-  filter {
-    name   = "tag:Name"
-    values = ["public_subnet"]
-  }
-  filter {
-    name   = "availability-zone"
-    values = ["us-west-2c"]
-  }
-}
-
 data "aws_ami" "ubuntu_22_arm64" {
   most_recent = true
 
@@ -67,5 +12,63 @@ data "aws_ami" "ubuntu_22_arm64" {
   }
 }
 
+data "tfe_outputs" "vpc" {
+  organization = "icestation-is"
+  workspace    = "vpc"
+}
 
+data "aws_subnet" "public_subnet_2a" {
+  vpc_id = data.tfe_outputs.vpc.outputs.vpc_output.main_vpc.vpc_output.vpc_id
+
+  filter {
+    name   = "tag:Name"
+    values = ["public_subnet"]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["us-west-2a"]
+  }
+}
+
+data "aws_subnet" "public_subnet_2b" {
+  vpc_id = data.tfe_outputs.vpc.outputs.vpc_output.main_vpc.vpc_output.vpc_id
+
+  filter {
+    name   = "tag:Name"
+    values = ["public_subnet"]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["us-west-2b"]
+  }
+}
+
+data "aws_subnet" "public_subnet_2c" {
+  vpc_id = data.tfe_outputs.vpc.outputs.vpc_output.main_vpc.vpc_output.vpc_id
+
+  filter {
+    name   = "tag:Name"
+    values = ["public_subnet"]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["us-west-2c"]
+  }
+}
+
+# for future use, commenting out since it is necessary for current needs# get my external IP address for adding to ingress rules in SGs
+# data "http" "my_ip" {
+#   url = "http://icanhazip.com"
+# }
+
+# data "terraform_remote_state" "vpc" {
+#   backend = "s3"
+
+#   config = {
+#     bucket         = "tfstate-icestation"
+#     key            = "vpc/terraform.state"
+#     region         = "us-west-2"
+#     dynamodb_table = "tfstate-icestation-dynamo"
+#   }
+# }
 
