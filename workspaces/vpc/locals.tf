9382916a6b902ffe6d982e072c65ff643e2e5ce6
Ryan Stickney <rstickney@icestation.is>
2024-08-31 15:19:10 -0700

    2 AZs instead of 3

--- workspaces/vpc/locals.tf
+++ workspaces/vpc/locals.tf
@@ -2,9 +2,9 @@ locals {
   vpc = {
     "main_vpc" = {
       cidr               = "10.141.0.0/16"
-      azs                = ["us-west-2a", "us-west-2b", "us-west-2c"]
-      private_subnets    = ["10.141.1.0/24", "10.141.2.0/24", "10.141.3.0/24"]
-      public_subnets     = ["10.141.101.0/24", "10.141.102.0/24", "10.141.103.0/24"]
+      azs                = ["us-west-2a", "us-west-2b", ]
+      private_subnets    = ["10.141.1.0/24", "10.141.2.0/24"]
+      public_subnets     = ["10.141.101.0/24", "10.141.102.0/24"]
       enable_nat_gateway = true
       single_nat_gateway = true
     }
