5d4ed7ec6595f50e07795c45e27e6d342336fa04
Ryan Stickney <153308973+rrstickney@users.noreply.github.com>
2024-08-31 15:20:33 -0700

    Vpc (#19)
    * adding outputs for vpc_id and subnets. and adding tags
    
    * adding more files
    
    * fixing a tagging issue and repetitive tagging
    
    * improving tagging and using a single nat gateway
    
    * 2 AZs instead of 3

--- workspaces/vpc/modules/aws/vpc/main.tf
+++ workspaces/vpc/modules/aws/vpc/main.tf
@@ -10,6 +10,7 @@ module "vpc" {
   enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
 
+
   tags                = var.tags
   private_subnet_tags = var.vpc_private_subnet_tags
   public_subnet_tags  = var.vpc_public_subnet_tags
